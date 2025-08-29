1520cd6ec05f9571391f1a4896ee5c8cda9aa5c6
fwdmirandataylor-hub <fwdmirandataylor@gmail.com>
2025-08-29 15:31:55 -0600

    ultimos cambios

--- public/js/login.js
+++ public/js/login.js
@@ -1,19 +1,21 @@
+import { getData } from "../services/fetch.js";
+
 const btnIngresar = document.getElementById("ingresar");
 const inputUsuario = document.getElementById("usuario");
 const inputContraseña = document.getElementById("contraseña");
 
-btnIngresar.addEventListener("click", () => {
+btnIngresar.addEventListener("click", async() => {
+  const usuarios = await getData("usuarios")
   const usuario = inputUsuario.value.trim();
   const contraseña = inputContraseña.value.trim();
+  const usuarioEncontrado = usuarios.find((usuarioR)=> usuarioR.nombre === usuario && usuarioR.password === contraseña)
 
-  if (usuario === "" || contraseña === "") {
-    alert("Por favor, completa todos los campos :)");
-    return;
+  if (!usuarioEncontrado) {
+      alert("no existe")
   }
-
-  if (usuario === "admin" && contraseña === "1234") {
-    alert("¡Bienvenida, Francella! Acceso concedido.");
-  } else {
-    alert("Usuario o contraseña incorrectos.");
+  if (usuarioEncontrado) {
+    alert("usuario iniciado")
+    // redirección a la otra página usando window.location.href
+    
   }
 });
